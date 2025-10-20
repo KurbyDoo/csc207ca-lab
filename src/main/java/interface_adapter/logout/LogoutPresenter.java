21fb36ff467bb02c154d9d037117483caf44393b
Leobstere <jerryzh163@gmail.com>
2025-10-20 14:17:23 -0400

    implemented LogoutPresenter

--- src/main/java/interface_adapter/logout/LogoutPresenter.java
+++ src/main/java/interface_adapter/logout/LogoutPresenter.java
@@ -38,19 +38,16 @@ public class LogoutPresenter implements LogoutOutputBoundary {
         // 1. get the LoggedInState out of the appropriate View Model,
         // 2. set the username in the state to the empty string
         // 3. firePropertyChanged so that the View that is listening is updated.
-        LoggedInState state = new LoggedInState();
-        this.loggedInViewModel.setState(state);
-        this.loggedInViewModel.firePropertyChange(username);
+        this.loggedInViewModel.getState().setUsername("");
+        this.loggedInViewModel.firePropertyChange("username");
 
 
         // TODO: have prepareSuccessView update the LoginState
         // 1. get the LoginState out of the appropriate View Model,
         // 2. set the username in the state to be the username of the user that just logged out,
         // 3. firePropertyChanged so that the View that is listening is updated.
-
-        LoginState sixSeven = new LoginState();
-        this.loginViewModel.setState(sixSeven);
-        this.loggedInViewModel.firePropertyChange(username);
+        this.loginViewModel.getState().setUsername("");
+        this.loggedInViewModel.firePropertyChange("username");
 
         // This code tells the View Manager to switch to the LoginView.
         this.viewManagerModel.setState(loginViewModel.getViewName());
